9873e9cd898e4a86122640e53266d8f45aaab5fd
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-04-17 18:05:01 +0200

    svolgimento milestone1

---
+++ js/script.js
@@ -0,0 +1,138 @@
+// Milestones
+// 1 Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout
+// 2 Coloriamo le icone per tipo
+// 3 Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+// Qui sotto trovate lo zip con screenshot, il logo e la struttura base js con l'oggetto icone e i colori
+
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	}
+];
+
+const colors = [
+    'blue',
+    'orange',
+    'purple'
+];
+
+// Milestone 1
+// Partendo dalla seguente struttura dati , 
+// mostriamo in pagina tutte le icone disponibili come da layout.
+
+let previousIcons = document.getElementById("elements");
+
+icons.forEach((element, index, array) => {
+
+
+    let printThisIcon = `
+    <li>
+        <div class="pattern">
+            <i class="${element.family} ${element.prefix}${element.name}"></i>
+        </div>
+    </li>
+    `;
+
+    previousIcons.innerHTML += printThisIcon;
+   
+});
+
+
+// Milestone 2
+// Coloriamo le icone per tipo
+
+// Milestone 3
+// Creiamo una select con i tipi di icone e usiamola per filtrare le icone
